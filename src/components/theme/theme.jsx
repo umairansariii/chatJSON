af9ec2fa713deb358ba2190101884a637f727497
umairansari <umairansari.work@gmail.com>
2023-11-09 02:15:48 +0500

    Added components/theme

---
+++ src/components/theme/theme.jsx
@@ -0,0 +1,10 @@
+// Style
+import './scss/theme.scss';
+
+export default function Theme() {
+    return (
+        <div className="dashboard-theme">
+            <h1>Theme</h1>
+        </div>
+    )
+};
