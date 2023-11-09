d5a4dad4251a5f9442edd82b916390f22fa0650c
umairansari <umairansari.work@gmail.com>
2023-11-09 19:02:50 +0500

    Refactor @core/converter/sms

--- src/utils/functions/sms.js
+++ src/utils/functions/sms.js
@@ -1,46 +1,55 @@
-import { getDateFormatted } from './date'
+function getAttributeValue(key, obj) {
+    const string = obj.match(`${key}="([^]*?)"`)[1];
 
-function getAttributeValue(name, data) {
-    const string = data.match(`${name}="([^]*?)"`)[1]
-
-    if (name == 'body') {
-        return (
-            string
-                .replace(/&gt;/g, '>')
-                .replace(/&lt;/g, '<')
-                .replace(/&amp;/g, '&')
-                .replace(/&quot;/g, '"')
-                .replace(/&apos;/g, "'")
-        )
-    } else {
+    if (key == "body") {
         return string
+            .replace(/&gt;/g, ">")
+            .replace(/&lt;/g, "<")
+            .replace(/&amp;/g, "&")
+            .replace(/&quot;/g, '"')
+            .replace(/&apos;/g, "'");
+    } else {
+        return string;
     }
 }
-function getCollection(object, id) {
+function getCollection(obj) {
     let name;
 
-    if ( getAttributeValue('type', object) == '2' ) {
-        name = 'You'
+    if (getAttributeValue("type", obj) == "2") {
+        name = "You";
     } else {
-        name = getAttributeValue('address', object)
+        name = getAttributeValue("address", obj);
     }
     return {
-        id: id,
         name: name,
-        date: getDateFormatted( +getAttributeValue('date', object) ),
-        body: getAttributeValue('body', object),
-    }
+        date: new Date(getAttributeValue("time", obj)),
+        body: getAttributeValue("body", obj),
+    };
 }
 export default function converter(buffer, filename) {
-    const array = buffer.match(/<sms[^]*?\/>/g)
-    const messages = []
+    /*
+        XML Structure:
+        <sms
+            address="xxx"
+            time="xxx"
+            type="<1|2>"
+            body="..."
+        />
+    */
+    const array = buffer.match(/<sms[^]*?\/>/g);
+    const messages = [];
 
+    // Collects all dataset from each message.
     for (let i = 0; i < array.length; i++) {
-        messages.push((getCollection(array[i], i)))
+        messages.push(getCollection(array[i]));
     }
+    // Sort by datetime and append id.
+    messages.sort((a, b) => a.date.getTime() - b.date.getTime());
+    messages.map((e, idx) => (e.id = idx));
+
     return {
         count: messages.length,
         name: filename,
         messages,
-    }
+    };
 }
