4a1532b9eb70f2e730fdb3bcb92979cb6dd32232
umairansari <umairansari.work@gmail.com>
2023-11-09 19:46:31 +0500

    Refactor @core/converter/messenger

--- src/utils/functions/messenger.js
+++ src/utils/functions/messenger.js
@@ -1,33 +1,54 @@
-import { getDateFormatted } from './date'
+function getCollection(obj) {
+    let body = "";
+    const arr = obj.match(/>([^]*?)(?=<)/g);
+    const date =
+        arr[arr.length - 1].slice(1, -2) + " " + arr[arr.length - 1].slice(-2);
 
-function getCollection(object, id) {
-    let body = ''
-    const arr = object.match(/>([^]*?)(?=<)/g)
-    const date = arr[arr.length-1].slice(1,-2) + ' ' + arr[arr.length-1].slice(-2)
-
-    for (let i = 7; i < arr.length-1; i++ ) {
-        if (arr[i] != '>') {
-            body += arr[i].slice(1) + '\n'
+    for (let i = 7; i < arr.length - 1; i++) {
+        if (arr[i] != ">") {
+            body += arr[i].slice(1) + "\n";
         }
     }
     return {
-        id: id,
         name: arr[1].slice(1),
-        date: getDateFormatted(date),
-        body: body? body: '<Media Omitted>',
-    }
+        date: new Date(date),
+        body: body ? body : "<Media Omitted>",
+    };
 }
 export default function converter(buffer, filename) {
-    const splitted = buffer.match(/role="main"[^]*$/g)[0]
-    const array = splitted.match(/<div\sclass="pam[^]*?[ap]m<\/div>/g)
-    const messages = []
+    // (!) Excluding unnecessary css data.
+    const splitted = buffer.match(/role="main"[^]*$/g)[0];
+    /*
+        HTML Structure:
+        <div class="_4t5n" role="main">
+            <div class="pam ...">
+                <div class="...">[name]</div>
+                <div class="...">
+                    <div>
+                        <div></div>
+                        <div>[body]</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div class="...">[time]</div>
+            </div>
+        </div>
+    */
+    const array = splitted.match(/<div\sclass="pam[^]*?[ap]m<\/div>/g);
+    const messages = [];
 
+    // Collects all dataset from each message.
     for (let i = 0; i < array.length; i++) {
-        messages.push((getCollection(array[i], i)))
+        messages.push(getCollection(array[i]));
     }
+    // Sort by datetime and append id.
+    messages.sort((a, b) => a.date.getTime() - b.date.getTime());
+    messages.map((e, idx) => (e.id = idx));
+
     return {
         count: messages.length,
         name: filename,
         messages,
-    }
+    };
 }
